1ab413529e088103b6f5719c541621e41cef2b49
zyuerugou <44331849+zyuerugou@users.noreply.github.com>
2020-02-19 21:54:35 +0800

    增加限定群聊，只监听该群消息

--- wechat_group.py
+++ wechat_group.py
@@ -9,6 +9,8 @@ import itchat
 from itchat.content import *
 import os
 import time
+import xlrd
+import xlwt
 
 # 文件临时存储页
 rec_tmp_dir = os.path.join(os.getcwd(), 'tmp/')
@@ -16,11 +18,22 @@ rec_tmp_dir = os.path.join(os.getcwd(), 'tmp/')
 # 存储数据的字典
 rec_msg_dict = {}
 
+# 特定的群聊id
+from_group = ''
+
+# 保存数据
+def saveData(msg_id, msg_from_user, msg_content, msg_create_time):
+    print('save data')
+
+# 解析消息
+def decodeMsg(msg_id, msg_from_user, msg_content, msg_create_time):
+    print('decode msg')
+
 # 群聊信息监听
 @itchat.msg_register([TEXT], isGroupChat=True)
 def information(msg):
-    rooms = itchat.search_chatrooms(name = '喵喵喵')
-    if len(rooms) ！= 0:
+    # print(msg['ToUserName'])
+    if msg['ToUserName'] == from_group:
         msg_id = msg['MsgId']
         msg_from_user = msg['ActualNickName']
         msg_content = ''
@@ -40,6 +53,7 @@ def information(msg):
                 'msg_content': msg_content
             }
         })
+        decodeMsg(msg_id, msg_from_user, msg_content, msg_create_time)
         print("群聊信息: ",msg_id, msg_from_user, msg_content, msg_create_time,msg_type)
 
 
@@ -47,4 +61,10 @@ if __name__ == '__main__':
     if not os.path.exists(rec_tmp_dir):
         os.mkdir(rec_tmp_dir)
     itchat.auto_login(hotReload = True)
+    group = itchat.get_chatrooms(update = True)
+    for g in group:
+        if g['NickName'] == '喵喵喵':
+            from_group = g['UserName']
+            break   
+    print(from_group)
     itchat.run()
